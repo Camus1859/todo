faa94a525a2adb9f482e8d62b3597356a137ba3b
Anderson <laventurepsu1@gmail.com>
2021-08-21 20:38:34 -0400

    added git attribute file

--- server/src/routers/todos.js
+++ server/src/routers/todos.js
@@ -4,20 +4,21 @@ const pool = require('../db/config');
 require('dotenv').config();
 
 router.post('/todo', (req, res) => {
+    console.log(req.body)
   const { title, list, description, date, priority, notes, daysuntil } = req.body;
 
-  pool.query(
-    'INSERT INTO todo_items (title, list, description, date, priority, notes, daysuntil) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING id',
-    [title, list, description, date, priority, notes, daysuntil],
-    (error, results) => {
-      if (error) {
-        console.log(error);
-        res.status(400);
-      }
-      res.status(201).send(JSON.stringify(results.rows[0].id));
-      console.log(results.rows);
-    }
-  );
+//   pool.query(
+//     'INSERT INTO todo_items (title, list, description, date, priority, notes, daysuntil) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING id',
+//     [title, list, description, date, priority, notes, daysuntil],
+//     (error, results) => {
+//       if (error) {
+//         console.log(error);
+//         res.status(400);
+//       }
+//       res.status(201).send(JSON.stringify(results.rows[0].id));
+//       console.log(results.rows);
+//     }
+//   );
 });
 
 router.get('/allTodos', async (req, res) => {
